7c32fe6ad43ff23e0a4e18aa5307b1d694a722f6
Emmanuel <mailkumah@gmail.com>
2022-06-29 00:50:26 +0000

    Add animation to hero.js

--- src/Components/Home/Sections/Hero.js
+++ src/Components/Home/Sections/Hero.js
@@ -1,20 +1,62 @@
 import React, { useRef, useEffect } from "react";
 import classes from "./Hero.module.css";
 import heroImg from "../../../../src/images/hero.png";
-import bulbIcon from "../../../images/bulbIcon.png";
+import { FcIdea } from "react-icons/fc";
 import { gsap } from "gsap";
 
 function Hero({ isMenuClicked }) {
   const heroTitleRef = useRef(null);
+  const subHeadingRef = useRef(null);
+  const subTitleRef = useRef(null);
+  const btnRef = useRef(null);
+  const imgRef = useRef(null);
+  const icon1Ref = useRef(null);
+  const icon2Ref = useRef(null);
 
   useEffect(() => {
-    gsap.from(heroTitleRef.current, {
-      duration: 1,
-      autoAlpha: 0,
-      ease: "none",
-      delay: 2,
-    });
+    gsap.fromTo(
+      [
+        heroTitleRef.current,
+        subHeadingRef.current,
+        subTitleRef.current,
+        btnRef.current,
+      ],
+      { opacity: 0, y: -20 },
+      { opacity: 1, duration: 1, y: 20, stagger: 2.5, delay: 1 }
+    );
+    gsap.fromTo(
+      imgRef.current,
+      {
+        x: -100,
+      },
+      {
+        duration: 1,
+        x: 100,
+        stagger: 1,
+        delay: 10,
+      }
+    );
+    gsap.fromTo(
+      icon1Ref.current,
+      { y: 18, opacity: 0 },
+      { duration: 1, y: -18, repeat: -1, yoyo: true, opacity: 1, delay: 12 }
+    );
+    gsap.fromTo(
+      icon2Ref.current,
+      { y: -18, opacity: 0 },
+      { duration: 1, y: 18, repeat: -1, yoyo: true, opacity: 1, delay: 12 }
+    );
   }, []);
+
+  const handleMouseEnter = () => {
+    btnRef.current.style.backgroundColor = "#FFCB77";
+
+    gsap.fromTo(
+      btnRef.current,
+      { y: 20 },
+      { duration: 1, y: -20, repeat: -1, yoyo: true }
+    );
+  };
 
   return (
     <div
@@ -24,37 +66,41 @@ function Hero({ isMenuClicked }) {
     >
       <section className={classes["hero__col"]}>
         <div className={classes["hero__desc"]}>
-          <h2 ref={heroTitleRef}>We give you the super power</h2>
-          <p>
+          <h2 ref={heroTitleRef} className={classes["hero__title"]}>
+            We give you the super power
+          </h2>
+          <p ref={subHeadingRef}>
             Solve everyday problems from finance, sports and health with ease{" "}
           </p>
-          <span className={classes["hero__subDesc"]}>
+          <p ref={subTitleRef} className={classes["hero__subDesc"]}>
             It will take you only few seconds
-          </span>
+          </p>
           <div>
-            <button className={classes["hero__btn"]}>Try it now</button>
+            <button
+              ref={btnRef}
+              className={classes["hero__btn"]}
+              onMouseEnter={handleMouseEnter}
+            >
+              Try it now
+            </button>
           </div>
         </div>
       </section>
       <section className={classes["hero__col"]}>
         <div className={classes["hero__icons"]}>
-          <img
-            src={bulbIcon}
-            alt={bulbIcon}
-            className={classes["hero__bulbIcon1"]}
-          />
-          <img
-            src={bulbIcon}
-            alt={bulbIcon}
-            className={classes["hero__bulbIcon2"]}
-          />
-          <img
-            src={bulbIcon}
-            alt={bulbIcon}
-            className={classes["hero__bulbIcon3"]}
-          />
+          <div ref={icon1Ref}>
+            <FcIdea />
+          </div>
+          <div ref={icon2Ref}>
+            <FcIdea />
+          </div>
         </div>
-        <img src={heroImg} alt="calcImg" className={classes["hero__img"]} />
+        <img
+          ref={imgRef}
+          src={heroImg}
+          alt="calcImg"
+          className={classes["hero__img"]}
+        />
       </section>
     </div>
   );
